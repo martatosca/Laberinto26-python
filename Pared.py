dc3a49c9e01e724b4d2d6db4d52e9ab71197f942
MARTA TOSCA PÉREZ <Marta.Tosca@alu.uclm.es>
2026-03-01 19:07:51 +0100

    Cambios en las clases

--- Pared.py
+++ Pared.py
@@ -3,5 +3,9 @@ from Hoja import Hoja
 class Pared(Hoja):
     def __init__(self):
         super().__init__()
+        
+    def entrar(self):
+        print("Has chocado contra una pared")
+        
     def __str__(self):
         return "Pared"
