eb61ca26166fce84d697efc007104077881ed291
MARTA TOSCA PÉREZ <Marta.Tosca@alu.uclm.es>
2026-03-01 18:46:13 +0100

    Implemento la clase Laberinto

--- Laberinto.py
+++ Laberinto.py
@@ -1,13 +1,19 @@
-from ElementoMapa import ElementoMapa
+from Contenedor import Contenedor
 
-class Laberinto(ElementoMapa):
+class Laberinto(Contenedor):
     def __init__(self):
         super().__init__()
-        self.habitaciones = []
+        self.habitaciones = {}
     
-    def agregarHabitacion(self, unaHab):
-        self.habitaciones.append(unaHab)
-    @property
-    def habitaciones(self):
-        return self._habitaciones
+    def agregar_Habitacion(self, habitacion):
+        self.habitaciones[habitacion.id] = habitacion
+        self.agregar_hijo(habitacion)
+        
+    def obtener_habitaciones(self,numero): #devuelve la habitacion con ese numero o None si no existe
+        return self.habitaciones.get(numero, None)
     
+    def entrar(self):
+        print("Has entrado al laberinto")
+    
+    def __str__(self):
+        return f"Laberinto con {len(self.habitaciones)} habitaciones"
